39f4bf1effe63acbad82efb8269cb09ba776d3fa
Miles Thompson <utunga@gmail.com>
2020-06-15 10:01:36 +1200

    move private keys to local_keys.py

--- cyclic_reciprocity_test.py
+++ cyclic_reciprocity_test.py
@@ -2,14 +2,12 @@ from web3 import Web3, HTTPProvider
 from contract_controller import ReservesContractControl, CyclicReciprocityContractControl
 from hashlib import sha256
 import time, binascii, random
-
+from local_keys import priv1, priv2, priv3
+ 
 def random_hash():
 	return sha256(''.join([random.choice('abcdefghijklmnopqrstuvwxyz1234567890') for _ in range(10)]).encode()).digest()
 
 if __name__ == '__main__':
-	priv1 = "fa9066494f87ea600d2047819f2ddab50f90e034ecf5bd279a55035ff8d555ee"
-	priv2 = "44ad5d96637fe21728a16b2aa7c4c35eb6c548f3f4a7724101e4747c2e7a1c21"
-	priv3 = "755324529362e2e77d0bc3433b20f18955fcc4d45a6ab14b6183f18f6d1d2d6c"
 
 	# NOTE: MUST BE RUNNING LOCAL GANACHE SERVER
 	w3 = Web3(HTTPProvider("http://localhost:7545"))
---
+++ local_keys.py
@@ -0,0 +1,10 @@
+# A few private keys needed for tests to un
+#
+# Run ganache and copy private keys from ganache into this file#
+#
+# Pick three random addresses, click the 'key' icon, get the private key
+# copy it into place below
+
+priv1 = "141b6170a350f6fee2715cce4de66da23baf49b6cdd9c08a8a63f47860e4316e"
+priv2 = "0d84e2943d41dd31972124ce29ecd5491f1602ed5e6eb93dbb3118deb888063d"
+priv3 = "a8ee3724eefa7e22c189184c259694820bd3a8de8a170c704189df27121dd018"
