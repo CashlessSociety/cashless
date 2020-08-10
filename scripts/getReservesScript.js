a76fde374047011163b1bd9a4a297c2ad1b258d2
superarius <superarius@gmail.com>
2020-08-10 10:26:03 -0400

    Minor alteration of getReserves script

--- scripts/getReservesScript.js
+++ scripts/getReservesScript.js
@@ -17,6 +17,10 @@ const ethers = require('ethers');
 	let cashlessABI = cashless.getCashlessContractABI('./../build/contracts/');
 	let contract = cashless.getContract(providerURL, cashlessAddress, cashlessABI, null);
 	let resp = await cashless.getReserves(contract, reservesAddress);
-	let readable = {balance: (resp["balance"]/ethers.utils.parseEther("1")).toString(), grossClaimed: (resp["grossClaimed"]/ethers.utils.parseEther("1")).toString(), grossDefaulted: (resp["grossDefaulted"]/ethers.utils.parseEther("1")).toString(), grossPaid: (resp["grossPaid"]/ethers.utils.parseEther("1")).toString()};
-	console.log("reserves info:", JSON.stringify(readable));
+	if (resp['owner']=='0x0000000000000000000000000000000000000000') {
+		console.log("reserves do not exist");
+	} else {
+		let readable = {balance: (resp["balance"]/ethers.utils.parseEther("1")).toString(), grossClaimed: (resp["grossClaimed"]/ethers.utils.parseEther("1")).toString(), grossDefaulted: (resp["grossDefaulted"]/ethers.utils.parseEther("1")).toString(), grossPaid: (resp["grossPaid"]/ethers.utils.parseEther("1")).toString()};
+		console.log("reserves info:", JSON.stringify(readable));
+	}
 })();
